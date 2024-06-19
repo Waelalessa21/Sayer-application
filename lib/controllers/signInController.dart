96c4242f3e5d4437a56b42c96ffe8eafb01925cf
Waelalessa21 <148769667+Waelalessa21@users.noreply.github.com>
2024-06-19 17:48:27 +0300

    Delete lib directory

--- lib/controllers/signInController.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import '../screens/OTP/verify_PhoneNum.dart';
-
-class NavigationController {
-  void navigateToOTP() {
-    Get.to(() => VerifyPhoneNumScreen());
-  }
-}
